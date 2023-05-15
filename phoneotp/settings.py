d8da60ddbb1e75a88eb199f4b4ec386afa20497a
lagrandecode <seunogunmolufirst1@gmail.com>
2023-05-15 23:05:49 +0100

    "commit"

--- phoneotp/settings.py
+++ phoneotp/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'register.User'
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'register',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
